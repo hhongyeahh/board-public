3e5ab0710098b5479a5c4e07dc52250513ad9606
hhongyeahh <hhongyeahh@gmail.com>
2024-09-04 22:11:04 +0900

    repostory

---
+++ src/main/java/com/boardpublic/repository/ArticleCommentRepository.java
@@ -0,0 +1,34 @@
+package com.boardpublic.repository;
+
+import com.boardpublic.domain.ArticleComment;
+import com.boardpublic.domain.QArticleComment;
+import com.querydsl.core.types.dsl.DateTimeExpression;
+import com.querydsl.core.types.dsl.StringExpression;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.util.List;
+
+@RepositoryRestResource
+public interface ArticleCommentRepository extends
+        JpaRepository<ArticleComment, Long>,
+        QuerydslPredicateExecutor<ArticleComment>,
+        QuerydslBinderCustomizer<QArticleComment> {
+    @Override //검색에 대한 세부 내용 구현
+    default void customize(QuerydslBindings bindings, QArticleComment root) {
+        bindings.excludeUnlistedProperties(true);
+        bindings.including(root.content, root.createdAt, root.createdBy);
+        bindings.bind(root.content).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.createdAt).first(DateTimeExpression::eq); // 동일 검사
+        bindings.bind(root.createdBy).first(StringExpression::containsIgnoreCase);
+    }
+
+    void deleteByIdAndUserAccount_UserId(Long articleCommentId, String userId);
+    List<ArticleComment> findByArticle_Id(Long articleId);
+
+
+}
+
---
+++ src/main/java/com/boardpublic/repository/UserAccountRepository.java
@@ -0,0 +1,10 @@
+package com.boardpublic.repository;
+
+import com.boardpublic.domain.UserAccount;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface UserAccountRepository extends JpaRepository<UserAccount, String> {
+
+}
