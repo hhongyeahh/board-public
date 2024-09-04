e9960d56735b7bf757ecdbfdc0b1ef5232703afd
hhongyeahh <hhongyeahh@gmail.com>
2024-09-04 22:09:22 +0900

    config

---
+++ src/main/java/com/boardpublic/config/JpaConfig.java
@@ -0,0 +1,26 @@
+package com.boardpublic.config;
+
+import com.boardpublic.dto.security.BoardPrincipal;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.Optional;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaConfig {
+    @Bean
+    public AuditorAware<String> auditorAware(){ // auditing에서 이름을 넣는 방법
+        return () -> Optional.ofNullable(SecurityContextHolder.getContext())
+                .map(SecurityContext::getAuthentication)
+                .filter(Authentication::isAuthenticated)
+                .map(Authentication::getPrincipal)
+                .map(BoardPrincipal.class::cast)
+                .map(BoardPrincipal::getUsername);
+    }
+}
